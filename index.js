c76ab39f571e7ffbe3df364a36de073dc4e9d5be
Anatoliy Gatt <anatoliy.gatt@aol.com>
2015-08-23 09:26:16 +0200

    Update version to 1.0.9

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * @module index
  * @description Entry point for deep-freeze-node module.
- * @version 1.0.8
+ * @version 1.0.9
  * @author Anatoliy Gatt [anatoliy.gatt@aol.com]
  * @copyright Copyright (c) 2015 Anatoliy Gatt
  * @license MIT
