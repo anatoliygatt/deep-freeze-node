ba9ccb75ecf064406f9973a273c799a6614dfaa4
Anatoliy Gatt <anatoliy.gatt@aol.com>
2015-09-24 10:29:12 +0200

    Replace " with ' in definitions of all strings in deep-freeze.js tests

--- test/deep-freeze.js
+++ test/deep-freeze.js
@@ -1,70 +1,70 @@
-var assert = require("assert");
-var freeze = require("../lib/deep-freeze");
+var assert = require('assert');
+var freeze = require('../lib/deep-freeze');
 
-describe("deep-freeze", function() {
-    describe("init", function() {
+describe('deep-freeze', function() {
+    describe('init', function() {
         var person = {};
         var expectedPerson = {};
 
         beforeEach(function() {
             person = {
-                name: "John",
-                surname: "Johnson",
+                name: 'John',
+                surname: 'Johnson',
                 age: 26,
                 address: {
-                    street: "1st Street",
-                    city: "Los Angeles",
-                    country: "USA"
+                    street: '1st Street',
+                    city: 'Los Angeles',
+                    country: 'USA'
                 },
                 vehicles: [
-                    "BMW",
-                    "Ferrari",
-                    "Lamborghini"
+                    'BMW',
+                    'Ferrari',
+                    'Lamborghini'
                 ]
             };
 
             expectedPerson = {
-                name: "John",
-                surname: "Johnson",
+                name: 'John',
+                surname: 'Johnson',
                 age: 26,
                 address: {
-                    street: "1st Street",
-                    city: "Los Angeles",
-                    country: "USA"
+                    street: '1st Street',
+                    city: 'Los Angeles',
+                    country: 'USA'
                 },
                 vehicles: [
-                    "BMW",
-                    "Ferrari",
-                    "Lamborghini"
+                    'BMW',
+                    'Ferrari',
+                    'Lamborghini'
                 ]
             }
         });
 
-        it("should not override any object properties", function() {
+        it('should not override any object properties', function() {
             person = freeze(person);
 
-            person.name = "Jack";
-            person.surname = "Jackson";
+            person.name = 'Jack';
+            person.surname = 'Jackson';
             person.age = 18;
             person.address = {
-                street: "Third Street",
-                city: "San Francisco",
-                country: "USA"
+                street: 'Third Street',
+                city: 'San Francisco',
+                country: 'USA'
             };
             person.vehicles = [
-                "Toyota"
+                'Toyota'
             ];
 
-            assert.deepEqual(person, expectedPerson, "any object properties should not be overridden");
+            assert.deepEqual(person, expectedPerson, 'any object properties should not be overridden');
         });
 
-        it("should not add new properties to an object", function() {
+        it('should not add new properties to an object', function() {
             person = freeze(person);
 
-            person.occupation = "Lawyer";
-            person.nickname = "JJ";
+            person.occupation = 'Lawyer';
+            person.nickname = 'JJ';
 
-            assert.deepEqual(person, expectedPerson, "new properties should not be added to an object");
+            assert.deepEqual(person, expectedPerson, 'new properties should not be added to an object');
         });
     });
 });
