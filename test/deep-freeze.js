5e7e44d9eb0f24ae5fc362973a74bd55f8fe7901
Benjamin Horsleben <benjamin@fizkerinc.dk>
2017-03-17 11:59:51 +0100

    Tests are now changing deep properties instead of only top-level properties

--- test/deep-freeze.js
+++ test/deep-freeze.js
@@ -46,14 +46,12 @@ describe('deep-freeze', function () {
             person.name = 'Jack';
             person.surname = 'Jackson';
             person.age = 18;
-            person.address = {
-                street: 'Third Street',
-                city: 'San Francisco',
-                country: 'USA'
-            };
-            person.vehicles = [
-                'Toyota'
-            ];
+            person.address.street = 'Third Street';
+            person.address.city = 'San Francisco';
+            person.address.country = 'USA';
+            person.vehicles[0] = 'Toyota';
+            delete person.vehicles[1]
+            delete person.vehicles[2]
 
             assert.deepEqual(person, expectedPerson, 'any object properties should not be overridden');
         });
